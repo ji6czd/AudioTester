28385035b90ebd9705d53d10815f167374f99883
KIRIAKE Masanori <seiken@rd01.net>
2024-01-24 08:21:05 +0900

    * 電圧と電流のピークホールドするようにした。 * 16サンプルの平均を取るようにした。

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,7 @@
 #include <ios>
 #include <variant>
 
-#define SPK_PIN D1
+#define SPK_PIN D10
 #define INA226_ADDR 0x40
 
 #define INA226REG_SHUNT_V 0x01
@@ -58,13 +58,17 @@ void setup() {
     tone(SPK_PIN, 2000, 300);
   } else {
     Serial.println("INA226 not found");
-    tone(SPK_PIN, 1000, 300);
+    while(1) {
+      tone(SPK_PIN, 1000, 300);
+      delay(1000);
+    }
   }
+  INA226_write(0x00, 0b0100010000000111);
   delay(500);
 }
 
-#define DELAY 5 // 測定間隔
-#define TH 0.3 // 測定限界
+#define DELAY 3 // 測定間隔
+#define TH 0.2 // 測定限界
 
 void printBar(int val) {
   constexpr uint8_t displayMultiprex=5;
@@ -91,23 +95,37 @@ void loop() {
   float amp = (int16_t)INA226_read(INA226REG_SHUNT_V) * 2.5f / SHUNT_R;
   float volt = INA226_read(INA226REG_BUS_V) * 1.25f / 1000.f;
   static float pamp = 0.0f;
-  static int holdtime = 0;
-  if (amp > pamp) {
+  static float pvolt = 0.0f;
+  static uint ampHoldTime = 0;
+  static uint voltHoldTime = 0;
+  if (amp > TH && amp > pamp) {
     pamp = amp;
-    holdtime = millis();
+    ampHoldTime = millis();
   }
-  if(holdtime && millis() > holdtime+3000) {
+  if(ampHoldTime && millis() > ampHoldTime+1000) {
     pamp = 0;
-    holdtime = 0;
+    ampHoldTime = 0;
+  }
+  if (volt > pvolt) {
+    pvolt = volt;
+    voltHoldTime = millis();
+  }
+  if(voltHoldTime && millis() > voltHoldTime+1000) {
+    pvolt = 0;
+    voltHoldTime = 0;
   }
     
 //		float amp = (float)random(1000) / 10.f;
 //		float volt = (float)random(10) / 2.f;
+  uint16_t pFreq = 0;
   uint16_t freq = 500+(amp*100.0);
-  Serial.printf("%.2fmA %.2fV\r", pamp, volt);
-  // printBar(int(amp));
-  if (amp > TH) {
-    tone(SPK_PIN, freq, DELAY-1);
+  Serial.printf("%.2fmA %.2fV\r", pamp, pvolt);
+  // printBar(int(pamp));
+  if (amp > TH && pFreq != freq) {
+    // tone(SPK_PIN, freq);
+    pFreq = freq;
+  } else {
+    noTone(SPK_PIN);
   }
   unsigned long elapse = millis() - t;
   if (elapse < DELAY) {
